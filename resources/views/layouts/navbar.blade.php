5b70a71606f4a460d5e2a1ba3887b4aef93733d7
Justin Orlean <justinorleanwidya@yahoo.com>
2023-01-09 17:16:53 +0800

    add authorization to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,29 +14,32 @@
         <li class="nav-item">
           <a class="nav-link" href="/rankings">Rankings</a>
         </li>
-        <li id="navbar-profile" class="nav-item ms-4">
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="navbar-profile-name"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Hi Erick
-            </button>
-            <ul class="dropdown-menu text-center" aria-labelledby="navbar-profile-name">
-              <li>
-                <a class="dropdown-item px-2" href="/user">Account</a>
-              </li>
-              <li>
-                <button id="navbar-logout-button" class="btn btn-danger">
-                  logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li id="navbar-button-login" class="nav-item ms-4">
-          <a href="./login.html" class="bg-white text-primary btn btn-secondary">
-            Sign In
-          </a>
-        </li>
+        @auth
+          <li id="navbar-profile" class="nav-item ms-4">
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="navbar-profile-name"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Hi {{ Auth::user()->username }}
+              </button>
+              <ul class="dropdown-menu text-center" aria-labelledby="navbar-profile-name">
+                <li>
+                  <a class="dropdown-item px-2" href="/user/{{ Auth::user()->id }}">Account</a>
+                </li>
+                <li>
+                  <button id="navbar-logout-button" class="btn btn-danger">
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </li>
+        @else
+          <li id="navbar-button-login" class="nav-item ms-4">
+            <a href="/login" class="bg-white text-primary btn btn-secondary">
+              Sign In
+            </a>
+          </li>
+        @endauth
       </ul>
     </div>
   </div>
