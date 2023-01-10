4f2cedb8150e956cbeaadc78e4488a2d78d1c975
AndreChristianto <101187450+AndreChristianto@users.noreply.github.com>
2023-01-10 10:23:48 +0700

    +index ranking (FIX), NFT Seeder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\NFT;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function viewHome() {
         $category = Category::all();
+        $popular = NFT::paginate(3);
+        $user = User::take(5)->orderBy('balance', 'DESC')->get();
 
-        return view('index')->with(['category' => $category]);
+        return view('index')->with(['category' => $category, 'popular' => $popular, 'user' => $user]);
     }
 }
--- database/seeders/NFTSeeder.php
+++ database/seeders/NFTSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\NFT;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class NFTSeeder extends Seeder
 {
@@ -15,37 +16,124 @@ class NFTSeeder extends Seeder
      */
     public function run()
     {
-        NFT::insert([
-            [
-                'name' => 'nft1',
-                'image' => 'https://picsum.photos/200/300',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem officia sint voluptatum magnam omnis nisi non aut similique tempore.',
-                'price' => 10000,
+        DB::table('nft')->insert([
+            'name' => "Bored Ape Yacht",
+            'description' => "NFT Bored Ape Yacht Club",
+            'image' => "image/NFT/1 - NFT Bored Ape Yacht Club-20211214165446.webp",
+            'price' => 100000,
+            'owner_id' => 1,
+            'creator_id' => 1,
+            'category_id' => 1,
+        ]);
 
-                'owner_id' => 1,
-                'creator_id' => 1,
-                'category_id' => 1
-            ],
-            [
-                'name' => 'nft2',
-                'image' => 'https://picsum.photos/200/300',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem officia sint voluptatum magnam omnis nisi non aut similique tempore.',
-                'price' => 12000,
+        DB::table('nft')->insert([
+            'name' => "Mutant Ape Yacht",
+            'description' => "Mutant Ape Yacht",
+            'image' => "image/NFT/2 - Mutant Ape Yacht.png",
+            'price' => 110000,
+            'owner_id' => 1,
+            'creator_id' => 1,
+            'category_id' => 1,
+        ]);
 
-                'owner_id' => 2,
-                'creator_id' => 2,
-                'category_id' => 2
-            ],
-            [
-                'name' => 'nft3',
-                'image' => 'https://picsum.photos/200/300',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem officia sint voluptatum magnam omnis nisi non aut similique tempore.',
-                'price' => 15000,
+        DB::table('nft')->insert([
+            'name' => "Kaizoku Mechanics",
+            'description' => "Kaizoku Mechanics",
+            'image' => "image/NFT/3 - Kaizoku Mechanics.jpg",
+            'price' => 120000,
+            'owner_id' => 2,
+            'creator_id' => 2,
+            'category_id' => 2,
+        ]);
 
-                'owner_id' => 3,
-                'creator_id' => 3,
-                'category_id' => 3
-            ]
+        DB::table('nft')->insert([
+            'name' => "NFT Cryptopunk",
+            'description' => "NFT Cryptopunk Collection",
+            'image' => "image/NFT/4 - nft-from-cryptopunk-collection-was-mistakenly-sold-for-less-than-0.01.jpg",
+            'price' => 130000,
+            'owner_id' => 2,
+            'creator_id' => 2,
+            'category_id' => 2,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "Muri",
+            'description' => "Muri by Haus",
+            'image' => "image/NFT/5 - Muri by Haus NFT.webp",
+            'price' => 100000,
+            'owner_id' => 3,
+            'creator_id' => 3,
+            'category_id' => 3,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "Ragnarok Meta NFT",
+            'description' => "Ragnarok Meta NFT",
+            'image' => "image/NFT/6 - Ragnarok Meta NFT.png",
+            'price' => 110000,
+            'owner_id' => 3,
+            'creator_id' => 3,
+            'category_id' => 3,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "OnChain Monkey",
+            'description' => "OnChain Monkey",
+            'image' => "image/NFT/7 - OnChain Monkey NFT.png",
+            'price' => 120000,
+            'owner_id' => 4,
+            'creator_id' => 4,
+            'category_id' => 4,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "MoonBird NFT",
+            'description' => "MoonBird NFT",
+            'image' => "image/NFT/8 - MoonBird NFT.webp",
+            'price' => 130000,
+            'owner_id' => 4,
+            'creator_id' => 4,
+            'category_id' => 4,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "Gutter Cat Gang",
+            'description' => "Gutter Cat Gang NFT",
+            'image' => "image/NFT/9 - Gutter Cat Gang.png",
+            'price' => 100000,
+            'owner_id' => 5,
+            'creator_id' => 5,
+            'category_id' => 5,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "DeadFellaz NFT",
+            'description' => "DeadFellaz NFT",
+            'image' => "image/NFT/10 - DeadFellaz NFT.webp",
+            'price' => 110000,
+            'owner_id' => 5,
+            'creator_id' => 5,
+            'category_id' => 5,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "Nyan Cat NFT",
+            'description' => "Nyan Cat NFT",
+            'image' => "image/NFT/11 - Nyan Cat.jpg",
+            'price' => 120000,
+            'owner_id' => 6,
+            'creator_id' => 6,
+            'category_id' => 6,
+        ]);
+
+        DB::table('nft')->insert([
+            'name' => "Panda NFT",
+            'description' => "Almost Famous Panda NFT",
+            'image' => "image/NFT/12 - Almost Famous Panda NFT.jpg",
+            'price' => 130000,
+            'owner_id' => 6,
+            'creator_id' => 6,
+            'category_id' => 6,
         ]);
     }
 }
